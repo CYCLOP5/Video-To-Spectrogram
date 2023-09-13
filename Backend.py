3edc968c8fcf9d59f2f0bd0dd90042633c03d669
Varun J <vnjhaveri@gmail.com>
2023-09-13 19:43:49 +0530

    Fixed identation errors

--- Backend.py
+++ Backend.py
@@ -20,19 +20,19 @@ from PyQt5 import uic
 fname = ""
 class UI(QMainWindow): 
 
-	def __init__(self):
-		super(UI, self).__init__()
-		uic.loadUi("dialog.ui", self)
-		self.button = self.findChild(QPushButton, "pushButton")
-		self.label = self.findChild(QLabel, "label")
-		self.button.clicked.connect(self.clicker)
-		self.show()
+    def __init__(self):
+        super(UI, self).__init__()
+        uic.loadUi("dialog.ui", self)
+        self.button = self.findChild(QPushButton, "pushButton")
+        self.label = self.findChild(QLabel, "label")
+        self.button.clicked.connect(self.clicker)
+        self.show()
 
-	def clicker(self):
-        global fname
-		fname = QFileDialog.getOpenFileName(self, "Open File", "c:\\gui\\images", "All Files (*);; MP4 Files (*.mp4)" )
-		if fname:
-			self.label.setText(fname[0])
+    def clicker(self):
+        fname
+        fname = QFileDialog.getOpenFileName(self, "Open File", "c:\\gui\\images", "All Files (*);; MP4 Files (*.mp4)" )
+        if fname:
+            self.label.setText(fname[0])
 
 
 # Initialize The App
